36528409683f57f244778f2e3b90707eeaae7cf2
viet-coding <viet.nguyen@bain.com>
2026-04-22 14:30:21 +0700

    Main code base

---
+++ replit.nix
@@ -0,0 +1,30 @@
+{pkgs}: {
+  deps = [
+    pkgs.freetype
+    pkgs.fontconfig
+    pkgs.xorg.libXtst
+    pkgs.libgbm
+    pkgs.cairo
+    pkgs.pango
+    pkgs.mesa
+    pkgs.xorg.libxcb
+    pkgs.xorg.libXrandr
+    pkgs.xorg.libXfixes
+    pkgs.xorg.libXext
+    pkgs.xorg.libXdamage
+    pkgs.xorg.libXcomposite
+    pkgs.xorg.libX11
+    pkgs.alsa-lib
+    pkgs.at-spi2-core
+    pkgs.libxkbcommon
+    pkgs.expat
+    pkgs.dbus
+    pkgs.libdrm
+    pkgs.cups
+    pkgs.at-spi2-atk
+    pkgs.atk
+    pkgs.nspr
+    pkgs.nss
+    pkgs.glib
+  ];
+}
